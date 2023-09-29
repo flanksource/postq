89816d5cde5548f6ed66df95aa3af814d402aa19
Aditya Thebe <thebeaditya@gmail.com>
2023-09-29 17:59:55 +0545

    chore: remove ignored payloads from notify router

--- pg/router.go
+++ pg/router.go
@@ -10,24 +10,12 @@ import (
 // based on the payload.
 type notifyRouter struct {
 	registry map[string]chan string
-
-	// payloads to skip
-	skip map[string]struct{}
 }
 
 func NewNotifyRouter() *notifyRouter {
 	return &notifyRouter{
 		registry: make(map[string]chan string),
-		skip:     make(map[string]struct{}),
 	}
-}
-
-func (t *notifyRouter) Skip(payloads ...string) *notifyRouter {
-	for i := range payloads {
-		t.skip[payloads[i]] = struct{}{}
-	}
-
-	return t
 }
 
 // RegisterRoutes creates a single channel for the given routes and returns it.
@@ -45,7 +33,7 @@ func (t *notifyRouter) Run(ctx postq.Context, channel string) {
 	go Listen(ctx, channel, eventQueueNotifyChannel)
 
 	for payload := range eventQueueNotifyChannel {
-		if _, ok := t.skip[payload]; ok || payload == "" {
+		if _, ok := t.registry[payload]; !ok || payload == "" {
 			continue
 		}
 
